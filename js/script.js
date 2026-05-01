7331a517ae91d191e0b9047ad1244ffbf78bc8a5
Luiz Henrique <santoshenriqueluiz1105@gmail.com>
2026-05-01 17:40:08 -0300

    teste

---
+++ js/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+   
+    const basePath = window.location.pathname.includes("/pages/") ? "../" : "";
+
+    fetch(basePath + "components/menu.html")
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("menu-container").innerHTML = data;
+
+        const toggle = document.querySelector(".menu-toggle");
+        const navLinks = document.querySelector(".nav-links");
+
+        toggle.addEventListener("click", () => {
+            navLinks.classList.toggle("active");
+        });
+    });
+
+    fetch(basePath + "components/footer.html")
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("footer-container").innerHTML = data;
+    });
+
+    const form = document.querySelector("form");
+    const msg = document.querySelector(".form-sucesso");
+
+    form.addEventListener("submit", function() {
+        msg.style.display = "block";
+    })
+
+});
+
+
